46c26ac71e6c4c0b2a1563f009a34c1cee7b4358
Alec Christensen <alec_christensen@hotmail.com>
2025-04-13 04:08:02 +0200

    Display projects from DB & add dynamic filter counts

--- WebApp/Controllers/ProjectsController.cs
+++ WebApp/Controllers/ProjectsController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Data.Entities;
 using Microsoft.AspNetCore.Authorization;
+using Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace WebApp.Controllers;
 
@@ -8,34 +9,18 @@ namespace WebApp.Controllers;
 [Route("projects")]
 public class ProjectsController : Controller
 {
-    [HttpGet("")]
-    public IActionResult Index()
+    private readonly ApplicationDbContext _context;
+
+    public ProjectsController(ApplicationDbContext context)
     {
-        // TODO: Byt ut mot data från databasen (EF Core) längre fram
-        // Tillfällig dummydata för visning av projektkort
-        var projects = new List<Project>
-        {
-            new Project {
-        ProjectName = "Website Redesign",
-        ClientName = "Gitlab Inc.",
-        Description = "Modernisera hela företagets webbplats med nytt UI och UX.",
-        StartDate = DateTime.Now.AddDays(-20),
-        EndDate = DateTime.Now.AddDays(15),
-        Budget = 75000,
-        Status = "started"
-    },
-    new Project {
-        ProjectName = "UI Kit för intern app",
-        ClientName = "Spotify",
-        Description = "Skapa ett återanvändbart komponentbibliotek för utvecklingsteamet.",
-        StartDate = DateTime.Now.AddMonths(-2),
-        EndDate = DateTime.Now.AddDays(-10),
-        Budget = 45000,
-        Status = "completed"
+        _context = context;
     }
-        };
 
-
+    [HttpGet("")]
+    public async Task<IActionResult> Index()
+    {
+        var projects = await _context.Projects.ToListAsync();
         return View(projects);
     }
 }
+
